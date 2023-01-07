0bf8459ea634de8cf41db56cb75d92e704bbd733
Francçois Pellissier <francois.pellissier@gmail.com>
2023-01-07 09:28:02 +0100

    feature: first version of app-image-resizer

---
+++ app-image-resizer/README.md
@@ -0,0 +1,76 @@
+# Intro 
+
+Deployment on AWS is done manually every time we update this repository
+
+**To do**: more details
+
+
+## Description
+Images are uplpoaded to directories (announcements, chat_messages, contributions, entourage_images, neighborhoods, organizations, recommandations, resources) by user
+Lambda function copies original file to source dir,
+then resizes it to 400x400 pixels max in root dir,
+then resizes it to 60x60 pixels max in small dir,
+
+
+## DEPLOY:
+
+> sls deploy -s dev
+
+> sls deploy -s staging
+
+> sls deploy -s prod
+
+Upload time: around s
+
+## External lib
+
+Sharp: https://sharp.pixelplumbing.com/changelog
+
+
+### Optionnal commands
+To touch existing files (**already done! DO NOT DO IT AGAIN**):
+
+>aws s3 cp --metadata {\"touched\":\"true\"}  s3://entourage-images-production/entourage_images/ s3://entourage-images-production/entourage_images/ --recursive
+
+Should be done for all subdir
+
+> aws s3 cp --metadata {\"touched\":\"true\"}  s3://entourage-images-development/entourage_images/testFPimageEntourage.jpeg s3://entourage-images-development/entourage_images/testFPimageEntourage.jpeg
+
+Copying Staging files for Dev
+
+> aws s3 cp s3://entourage-images-development/staging/source/ s3://entourage-images-development/ --recursive
+
+> serverless config credentials --provider aws --key /*key*/ --secret /*secret*/
+
+> serverless --version
+
+> serverless create --template aws-nodejs --path resize_app_upload
+
+> serverless invoke local --function resize-app-upload -p ./test/event.json --stage prod
+
+> serverless invoke local --function resize-app-upload -p ./test/event_dev.json --stage dev
+
+## TO DO 
+- record action into BDD
+- handle PNG files
+
+aws s3 cp s3://entourage-avatars-production-thumb/staging/300x300/testFPavatarEntourage.jpeg s3://entourage-images-production/entourage_images/testFPimageEntourage.jpeg
+
+aws s3 cp s3://entourage-avatars-production-thumb/staging/300x300/testFPavatarEntourage.jpeg s3://entourage-images-development/staging/entourage_images/testFPimageEntourage.jpeg
+
+
+aws s3 rm s3://entourage-images-development/staging/source/entourage_images/testFPimageEntourage.jpeg
+aws s3 rm s3://entourage-images-development/staging/medium/entourage_images/testFPimageEntourage.jpeg
+aws s3 rm s3://entourage-images-development/staging/small/entourage_images/testFPimageEntourage.jpeg
+aws s3 rm s3://entourage-images-development/staging/entourage_images/testFPimageEntourage.jpeg
+
+aws s3 rm s3://entourage-images-development/source/staging/entourage_images/testFPimageEntourage.jpeg
+aws s3 rm s3://entourage-images-development/small/staging/entourage_images/testFPimageEntourage.jpeg
+
+aws s3 cp s3://entourage-avatars-production-thumb/staging/300x300/testFPavatarEntourage.jpeg s3://entourage-images-development/staging/entourage_images/testFPimageEntourage.jpeg
+
+aws s3 ls entourage-images-development/staging/entourage_images/testFPimageEntourage.jpeg
+aws s3 ls entourage-images-development/staging/source/entourage_images/testFPimageEntourage.jpeg
+aws s3 ls entourage-images-development/staging/medium/entourage_images/testFPimageEntourage.jpeg
+aws s3 ls entourage-images-development/staging/small/entourage_images/testFPimageEntourage.jpeg
+ 
---
+++ app-image-resizer/handler.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+const sharp  = require('sharp')
+
+const targetBucket=process.env.target_bucket
+const targetSize = process.env.target_size
+const targetDir=process.env.target_dir_prefix
+const targetSmallSize = process.env.small_target_size
+const targetSmallDir=process.env.small_target_dir_prefix
+const dirPrefix=process.env.dir_prefix
+const sourceDir=process.env.source_dir
+const requestString = process.env.request_string
+
+/*
+  * Transformations begin
+  */
+async function transform(data, new_size)
+{
+  var image = sharp(data);
+  return image
+    .metadata()
+    .then( function(size) {
+      const newratio = Math.max(size.width/new_size, size.height/new_size, 1);
+      const neww = Math.floor(size.width/newratio);
+      const newh = Math.floor(size.height/newratio);
+      console.debug('Resizing from ' + size.width + 'x' + size.height + " to "+ neww + 'x' + newh+ ' using ratio=' +newratio) ;
+      return image.resize(neww, newh)
+        .toBuffer() ;
+    })
+}
+
+module.exports.resizeAppUpload = async (event, context) => {
+  console.log ('******** START *********' + targetBucket + ' '+ targetSize + ' for '+ targetDir + ' ') ;
+  //console.log("Event: "+ JSON.stringify(event, null, 2));
+  const requestUser = event.Records[0].userIdentity.principalId;
+  //console.debug("Source: "+ requestUser);
+  if(requestUser.indexOf(requestString)!=-1) {
+    console.log("Internal event: exiting.")
+    return
+  }
+  
+  try {
+    const srcBucket = event.Records[0].s3.bucket.name
+    //console.debug("srcBucket="+srcBucket);
+    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " ")) ;
+    //console.debug("srcKey="+srcKey);
+
+    const typeMatch = srcKey.match(/\.([^.]*)$/) ;
+    //TODO check if typematch a au moins 2 parts
+    //console.info("typeMatch="+typeMatch);
+        
+    const imageType = typeMatch[1] ;
+  
+    const response = await s3
+      .getObject({ Bucket: srcBucket, Key: srcKey })
+      .promise() // `.promise()` is unconventional and specific to aws-sdk
+    console.debug('Getting file from '+srcKey +' on '+srcBucket)
+
+    let targetKey  = srcKey.replace(dirPrefix, sourceDir)
+    
+    await s3.copyObject({
+        Bucket: targetBucket, 
+        Key: targetKey, 
+        CopySource: srcBucket + "/" + srcKey
+      })
+      .promise()
+    console.log("Saving to " + targetKey + " on " + targetBucket)
+    
+    targetKey  = srcKey.replace(dirPrefix, targetSmallDir)
+    const resizedBuffer2 = await transform(response.Body, targetSmallSize)
+    //if(resizedBuffer2) console.info('Saving this buffer...'+resizedBuffer) 
+    const res2 = await s3
+      .putObject({
+          Bucket: targetBucket,
+          Key: targetKey,
+          Body: resizedBuffer2
+        })
+      .promise()
+    //console.log("Res: "+JSON.stringify(res2, null, 2))
+    console.log("Saving to " + targetKey + " on " + targetBucket)
+
+
+    targetKey  = srcKey.replace(dirPrefix, targetDir)
+    const resizedBuffer = await transform(response.Body, targetSize)
+    //if(resizedBuffer) console.info('Saving this buffer...'+resizedBuffer) 
+    const res = await s3
+      .putObject({
+          Bucket: targetBucket,
+          Key: targetKey,
+          Body: resizedBuffer
+        })
+      .promise()
+    //console.log("Res: "+JSON.stringify(res, null, 2))
+    console.log("Saving to " + targetKey + " on " + targetBucket)
+
+    /*
+     * Transformations end
+     */
+
+  } catch (error) {
+    console.error(error)
+  }
+  console.debug ('********** END ************')
+}
