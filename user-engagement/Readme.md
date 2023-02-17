466c445a3a19d60eed2a0357e923ebab9fb30232
François Pellissier <francois.pellissier@gmail.com>
2023-02-17 23:38:16 +0100

    feature (user-engagement) : new way of consolidating engaged users

--- user-engagement/Readme.md
+++ user-engagement/Readme.md
@@ -65,21 +65,66 @@ Process time is around 190s
 Avec la nouvelle app, on aimerait qu'un utilisateur soit considéré comme engagé si il fait au moins l'une de ces actions :
 
 ### envoyer un message privé (messagerie)
-### publier un post (sur page groupe ou événement)
-### commenter un post (sur page groupe ou événement)
-### créer une contribution ou demande
-### rejoindre une contribution ou demande [users sur ancienne app]
-### créer un événement
 
+``select  date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id,  
+coalesce(case when sender_addresses.country = 'FR' then sender_addresses.postal_code end, 'unknown') 
+from chat_messages  
+join entourages on messageable_type = 'Entourage' and messageable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type = 'conversation'  
+join users sender on sender.id = chat_messages.user_id  
+left join addresses sender_addresses on sender_addresses.id = address_id  
+where message_type = 'text'``
+### publier/commenter un post (sur page événement ou action)
+
+``select date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id, coalesce(case when country = 'FR' then postal_code end, 'unknown') 
+from chat_messages  
+join entourages on messageable_type = 'Entourage' and messageable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type in ('action', 'outing') 
+where message_type = 'text'``
+### publier/commenter un post (sur page groupe )
+
+``select date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id, coalesce(case when country = 'FR' then postal_code end, 'unknown') 
+    from chat_messages  
+    join neighborhoods n  on messageable_type = 'Neighborhood' and messageable_id = n.id 
+    where message_type = 'text' and n.status!='deleted'``
+### créer une contribution ou demande ou un événement
+
+``select date(timezone('Europe/Paris', created_at at time zone 'UTC')), user_id, coalesce(case when country = 'FR' then postal_code end, 'unknown') 
+from entourages where entourages.community = 'entourage' and entourages.group_type in ('action', 'outing')";``
+
+### rejoindre une contribution ou demande [users sur ancienne app]
+``select date(timezone('Europe/Paris', coalesce(accepted_at, join_requests.created_at) at time zone 'UTC')), join_requests.user_id, 
+coalesce(case when country = 'FR' then postal_code end, 'unknown') 
+from join_requests  
+join entourages on joinable_type = 'Entourage' and joinable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type ='action' 
+where join_requests.status='accepted' and message is not null and trim(message, ' \\n') != ''``
 
 ### participer à un événement
-### créer un groupe
-SELECT user_id, created_at, postal_code
-FROM public.neighborhoods
-where status!='deleted';
 
-## rejoindre un groupe (ATTENTION il faut exclure le groupe de voisins associé par défaut à tout utilisateur se connectant à la v8)
-## voir au moins 3 contenus pédagogiques
+``select date(timezone('Europe/Paris', coalesce(accepted_at, join_requests.created_at) at time zone 'UTC')), join_requests.user_id, 
+coalesce(case when country = 'FR' then postal_code end, 'unknown') 
+from join_requests  
+join entourages on joinable_type = 'Entourage' and joinable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type = 'outing'
+where join_requests.status='accepted'``
+
+### créer un groupe
+
+``SELECT user_id, created_at, postal_code
+FROM public.neighborhoods
+where status!='deleted' ;``
+
+### rejoindre un groupe (ATTENTION il faut exclure le groupe de voisins associé par défaut à tout utilisateur se connectant à la v8)
+``with
+  users_with_groups as (
+    select user_id, date_trunc('day', accepted_at) as date, 
+	sum(count(distinct joinable_id)) over (partition by user_id order by  date_trunc('day', accepted_at) rows between unbounded preceding and current row) as total 
+	from join_requests jr where joinable_type = 'Neighborhood' and status='accepted' and role='member'
+	group by 1, 2
+  )
+select g.user_id, g.date, a.postal_code 
+from users_with_groups g
+inner join users u on u.id=g.user_id 
+inner join addresses a on u.address_id = a.id where total>1;``
+
+### voir au moins 3 contenus pédagogiques
 
 ``with
   user_watchlist as (
--- user-engagement/handler.js
+++ user-engagement/handler.js
@@ -47,17 +47,23 @@ module.exports.aggregate = async () => {
     const result1 = await pgPool.query("select max(denorm_daily_engagements.date) as max_date from denorm_daily_engagements");
     let max_date = result1.rows[0].max_date;
     
-    let result_insertquery2 = "insert into denorm_daily_engagements (date, user_id, postal_code) \
-      select date(timezone('Europe/Paris', created_at at time zone 'UTC')), user_id, coalesce(case when country = 'FR' then postal_code end, 'unknown') \
+    let insertqueryCreateActionEvent = "insert into denorm_daily_engagements (date, user_id, postal_code) \
+      select date(timezone('Europe/Paris', created_at at time zone 'UTC')), user_id, coalesce(postal_code, 'unknown') \
       from entourages where entourages.community = 'entourage' and entourages.group_type in ('action', 'outing')";
     
-    let result_insertquery3 = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
-      select date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id, coalesce(case when country = 'FR' then postal_code end, 'unknown') \
-      from chat_messages  \
-      join entourages on messageable_type = 'Entourage' and messageable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type in ('action', 'outing') \
-      where message_type = 'text'  ";
-    
-      let result_insertquery4 = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
+    let insertqueryPostInActionEvent = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
+    select date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id, coalesce(postal_code, 'unknown') \
+    from chat_messages  \
+    join entourages on messageable_type = 'Entourage' and messageable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type in ('action', 'outing') \
+    where message_type = 'text'  ";
+  
+    let insertqueryPostInGroup = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
+    select date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id, coalesce(postal_code, 'unknown') \
+    from chat_messages  \
+    join neighborhoods n  on messageable_type = 'Neighborhood' and messageable_id = n.id \
+    where message_type = 'text' and n.status!='deleted'  ";
+  
+    let insertqueryPostMessages = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
         select  date(timezone('Europe/Paris', chat_messages.created_at at time zone 'UTC')), chat_messages.user_id,  \
         coalesce(case when sender_addresses.country = 'FR' then sender_addresses.postal_code end, 'unknown') \
         from chat_messages  \
@@ -65,58 +71,91 @@ module.exports.aggregate = async () => {
         join users sender on sender.id = chat_messages.user_id  \
         left join addresses sender_addresses on sender_addresses.id = address_id  \
         where message_type = 'text' ";
+      
+    let insertqueryJoinAction = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
+    select date(timezone('Europe/Paris', coalesce(accepted_at, join_requests.created_at) at time zone 'UTC')), join_requests.user_id, \
+    coalesce(case when country = 'FR' then postal_code end, 'unknown') \
+    from join_requests  \
+    join entourages on joinable_type = 'Entourage' and joinable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type='action' \
+    where join_requests.status='accepted' and message is not null and trim(message, ' \\n') != ''";
+
+    let insertqueryJoinEvent = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
+    select date(timezone('Europe/Paris', coalesce(accepted_at, join_requests.created_at) at time zone 'UTC')), join_requests.user_id, \
+    coalesce(case when country = 'FR' then postal_code end, 'unknown') \
+    from join_requests  \
+    join entourages on joinable_type = 'Entourage' and joinable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type = 'outing' \
+    where join_requests.status='accepted' ";
+
+    let insertqueryWatchResources = "with user_watchlist as ( \
+      select user_id, date_trunc('day', created_at) as date, \
+      sum(count(distinct resource_id)) over (partition by user_id order by  date_trunc('day', created_at) rows between unbounded preceding and current row) as total \
+      from users_resources ur where watched =true group by 1, 2)\
+      insert into denorm_daily_engagements  (date, user_id, postal_code) \
+      select date, user_id, 'unknown' from user_watchlist where total >=3";
+
+    let insertqueryCreationGroup = " insert into denorm_daily_engagements  (date, user_id, postal_code) \
+      SELECT date(timezone('Europe/Paris', created_at at time zone 'UTC')), user_id, postal_code \
+      FROM public.neighborhoods where status!='deleted'";
     
-        let result_insertquery5 = "insert into denorm_daily_engagements  (date, user_id, postal_code) \
-        select date(timezone('Europe/Paris', coalesce(requested_at, join_requests.created_at) at time zone 'UTC')), join_requests.user_id, \
-        coalesce(case when country = 'FR' then postal_code end, 'unknown') \
-        from join_requests  \
-        join entourages on joinable_type = 'Entourage' and joinable_id = entourages.id and entourages.community = 'entourage' and entourages.group_type in ('action', 'outing') \
-        where (group_type = 'outing' or (message is not null and trim(message, ' \\n') != ''))";
+    let insertQueryUsersWithGroups = "with users_with_groups as ( \
+      select user_id, date_trunc('day', accepted_at) as date,  \
+      sum(count(distinct joinable_id))  \
+      over (partition by user_id order by  date_trunc('day', accepted_at) rows between unbounded preceding and current row) as total  \
+      from join_requests jr  \
+      where joinable_type = 'Neighborhood' and status='accepted' and role='member' \
+      group by 1, 2) \
+      insert into denorm_daily_engagements  (user_id, date, postal_code) \
+      select g.user_id, g.date, coalesce(case when country = 'FR' then postal_code end, 'unknown')  \
+      from users_with_groups g \
+      inner join users u on u.id=g.user_id  \
+      inner join addresses a on u.address_id = a.id where total>1 "
 
-        let result_insertqueryWatchResources = "with user_watchlist as ( \
-          select user_id, date_trunc('day', created_at) as date, \
-          sum(count(distinct resource_id)) over (partition by user_id order by  date_trunc('day', created_at) rows between unbounded preceding and current row) as total \
-          from users_resources ur where watched =true group by 1, 2)\
-        insert into denorm_daily_engagements  (date, user_id, postal_code) \
-        select date, user_id, 'unknown' from user_watchlist where total >=3";
-
-        let result_insertqueryCreationGroup = " insert into denorm_daily_engagements  (date, user_id, postal_code) \
-          SELECT date(timezone('Europe/Paris', created_at at time zone 'UTC')), user_id, postal_code \
-          FROM public.neighborhoods where status!='deleted'";
-
-      if(max_date != null) {
-        result_insertquery2 += "and created_at >= '"+max_date.toISOString() + "' ";
-        result_insertqueryCreationGroup += "and created_at >= '"+max_date.toISOString() + "' ";
-        result_insertquery3 += "and chat_messages.created_at >= '"+max_date.toISOString() + "' ";
-        result_insertquery4 += "and chat_messages.created_at >= '"+max_date.toISOString() + "' ";
-        result_insertquery5 += "and coalesce(requested_at, join_requests.created_at) >= '"+max_date.toISOString() + "' ";
-        result_insertqueryWatchResources += "and date >= '"+max_date.toISOString() + "' ";
+    if(max_date != null) {
+      insertqueryCreateActionEvent += " and created_at >= '"+max_date.toISOString() + "' ";
+      insertqueryCreationGroup += " and created_at >= '"+max_date.toISOString() + "' ";
+      insertqueryPostInActionEvent += " and chat_messages.created_at >= '"+max_date.toISOString() + "' ";
+      insertqueryPostInGroup += " and chat_messages.created_at >= '"+max_date.toISOString() + "' ";
+      insertqueryPostMessages += " and chat_messages.created_at >= '"+max_date.toISOString() + "' ";
+      insertqueryJoinAction += " and coalesce(accepted_at, join_requests.created_at) >= '"+max_date.toISOString() + "' ";
+      insertqueryJoinEvent += " and coalesce(accepted_at, join_requests.created_at) >= '"+max_date.toISOString() + "' ";
+      insertqueryWatchResources += " and date >= '"+max_date.toISOString() + "' ";
+      insertQueryUsersWithGroups += " and date >= '"+max_date.toISOString() + "' ";
     }
+    
     const conflict_string = " on conflict (date, user_id, postal_code) do nothing"
-    result_insertquery2 += conflict_string;
-    result_insertquery3 += conflict_string;
-    result_insertquery4 += conflict_string;
-    result_insertquery5 += conflict_string;
-    result_insertqueryWatchResources += conflict_string;
-    result_insertqueryCreationGroup += conflict_string;
+    insertqueryCreateActionEvent += conflict_string;
+    insertqueryPostInActionEvent += conflict_string;
+    insertqueryPostInGroup += conflict_string;
+    insertqueryPostMessages += conflict_string;
+    insertqueryJoinAction += conflict_string;
+    insertqueryJoinEvent += conflict_string;
+    insertqueryWatchResources += conflict_string;
+    insertqueryCreationGroup += conflict_string;
+    insertQueryUsersWithGroups += conflict_string
 
-    const result2 = await pgPool.query(result_insertquery2)
+    const result2 = await pgPool.query(insertqueryCreateActionEvent)
     let rowCount = result2.rowCount
-    const result3 = await pgPool.query(result_insertquery3)
-    rowCount += result3.rowCount
-    const result4 = await pgPool.query(result_insertquery4)
+    const resultPostInActionEvent = await pgPool.query(insertqueryPostInActionEvent)
+    rowCount += resultPostInActionEvent.rowCount
+    const resultPostInGroup = await pgPool.query(insertqueryPostInGroup)
+    rowCount += resultPostInGroup.rowCount
+    const result4 = await pgPool.query(insertqueryPostMessages)
     rowCount += result4.rowCount
-    const result5 = await pgPool.query(result_insertquery5)
+    const result50 = await pgPool.query(insertqueryJoinAction)
+    rowCount += result50.rowCount
+    const result5 = await pgPool.query(insertqueryJoinEvent)
     rowCount += result5.rowCount
-    const resultWatchResources = await pgPool.query(result_insertqueryWatchResources)
+    const resultWatchResources = await pgPool.query(insertqueryWatchResources)
     rowCount += resultWatchResources.rowCount
-    const resultCreationGroup = await pgPool.query(result_insertqueryCreationGroup)
+    const resultCreationGroup = await pgPool.query(insertqueryCreationGroup)
     rowCount += resultCreationGroup.rowCount
+    const resultUsersWithGroups = await pgPool.query(insertQueryUsersWithGroups)
+    rowCount += resultUsersWithGroups.rowCount
 
-    let start = "beginning";
+    /*let start = "beginning";
     if(max_date != null ) start = max_date.toISOString();
 
-    /*await publishMessage(process.env.channelId, 
+    await publishMessage(process.env.channelId, 
       "User Engagement Consolidation: OK on stage:"+process.env.stage_name+"\n "
       + rowCount + " new daily engagements from "+start+" to now."
     );*/
@@ -141,4 +180,4 @@ module.exports.aggregate = async () => {
       }),
     };
   }
-};
+}
